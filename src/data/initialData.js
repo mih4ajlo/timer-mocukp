c605b43211288585a959c94173d68c9701760624
mihajlo <mihajlo88@gmail.com>
2019-04-17 17:49:55 +0200

    izdvojene pocetne vrednosti i vrednosti za  podesavanja

---
+++ src/data/initialData.js
@@ -0,0 +1,78 @@
+ //inicijalna vremena sluze za resetovanje
+ // neke druge promenljive su ogranicenja, a neke skladiste stanja
+ // promeniti imena promenljivih da budu prikladnije
+
+
+ //initial values, not nulls
+
+ //vremena da se cuvaju u sekundama
+
+ export const initialData = {
+    
+        minPlayersNo: 2,
+        playersNo: 2,
+        players: [],
+
+        totalMoves: 0,
+
+        playersTime: [
+        {
+             seconds:0, 
+             playerNo: 0,
+             basicTimeOver:false,
+             basicTime: 1,
+             byoyomiTime: 10,
+             byoyomiPeriods: 3,
+             pause: true,
+             moveNo: 0,
+        },
+        {
+             seconds:0, 
+             playerNo: 1,
+             basicTimeOver:false,
+             basicTime: 1,
+             byoyomiTime: 10,
+             byoyomiPeriods: 3,
+             pause: true,
+             moveNo: 0,
+        },
+        {
+             seconds:0, 
+             playerNo: 2,
+             basicTimeOver:false,
+             basicTime: 1,
+             byoyomiTime: 10,
+             byoyomiPeriods: 3,
+             pause: true,
+             moveNo: 0,
+        },
+     ],
+
+     //initial settings
+     basicTime: 10,
+     byoyomiTime: 10,
+     byoyomiPeriods: 3,
+     
+
+ }
+
+
+ export const settings = {
+
+    osnovnoVreme: [
+         { "name": '5', "id": 1 },
+         { "name": '10', "id": 2 },
+         { "name": '15', "id": 3 },
+         { "name": '30', "id": 4 },
+         { "name": '45', "id": 5 },
+         { "name": '60', "id": 6 },
+     ],
+     byoyomiVreme: [
+         { "name": '10', "id": '10s' },
+         { "name": '20', "id": '20s' },
+         { "name": '30', "id": '30s' },
+         { "name": '45', "id": '45s' },
+         { "name": '60', "id": '60s' }
+     ],
+
+ };
